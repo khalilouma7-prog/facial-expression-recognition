b0203aa1527c0b6655d042839ff603e364af0c67
oumaima <khalilouma7@gmail.com>
2026-04-09 02:52:22 +0200

    lmsegi

---
+++ archive/train_model.py
@@ -0,0 +1,110 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+
+# path du dataset
+train_dir = "archive/train"
+test_dir = "archive/test"
+
+# paramètres
+img_size = 48
+batch_size = 32
+
+# Augmentation pour l'entraînement (le modèle devient plus "robuste")
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=30,
+    shear_range=0.3, # d'inclinaison
+    zoom_range=0.3,
+    horizontal_flip=True,
+    fill_mode='nearest'
+)
+
+# Simple normalisation pour la validation (on ne transforme pas les tests)
+val_datagen = ImageDataGenerator(rescale=1./255)
+
+# Chargement (adaptez les chemins vers vos dossiers)
+train_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=(img_size, img_size),
+    color_mode='grayscale',
+    batch_size=batch_size,
+    class_mode='categorical',
+    shuffle=True
+)
+
+val_generator = val_datagen.flow_from_directory(
+    test_dir,
+    target_size=(img_size, img_size),
+    color_mode='grayscale',
+    batch_size=batch_size,
+    class_mode='categorical'
+)
+model = Sequential()
+
+# Block-1
+
+model.add(Conv2D(32,(3,3),padding='same',kernel_initializer='he_normal',input_shape=(img_rows,img_cols,1)))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(Conv2D(32,(3,3),padding='same',kernel_initializer='he_normal',input_shape=(img_rows,img_cols,1)))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.2))
+
+# Block-2 
+
+model.add(Conv2D(64,(3,3),padding='same',kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(Conv2D(64,(3,3),padding='same',kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.2))
+
+# Block-3
+
+model.add(Conv2D(128,(3,3),padding='same',kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(Conv2D(128,(3,3),padding='same',kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.2))
+
+# Block-4 
+
+model.add(Conv2D(256,(3,3),padding='same',kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(Conv2D(256,(3,3),padding='same',kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.2))
+
+# Block-5
+
+model.add(Flatten())
+model.add(Dense(64,kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.5))
+
+# Block-6
+
+model.add(Dense(64,kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.5))
+
+# Block-7
+
+model.add(Dense(num_classes,kernel_initializer='he_normal'))
+model.add(Activation('softmax'))
+
+print(model.summary())
